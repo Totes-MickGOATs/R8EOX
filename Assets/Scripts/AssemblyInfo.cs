7ea417ccf189b5615d4c12365a85edf8abdd0694
Adam McCune <totesmickgoats@Mac.home>
2026-03-23 14:22:14 -0600

    feat: grant PlayMode tests access to internal types via InternalsVisibleTo

--- Assets/Scripts/AssemblyInfo.cs
+++ Assets/Scripts/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("R8EOX.Editor")]
 [assembly: InternalsVisibleTo("R8EOX.Tests.EditMode")]
+[assembly: InternalsVisibleTo("R8EOX.Tests.PlayMode")]
