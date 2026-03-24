7ef1dfe1789f5c25baea79f0138d63c11cd3527a
Adam McCune <totesmickgoats@Mac.home>
2026-03-23 21:01:59 -0600

    feat: add DiagChannel enum for diagnostics system

---
+++ Assets/Scripts/Diagnostics/DiagChannel.cs
@@ -0,0 +1,17 @@
+namespace R8EOX.Diagnostics
+{
+    public enum DiagChannel
+    {
+        App,
+        Session,
+        Menu,
+        UI,
+        Vehicle,
+        Track,
+        Race,
+        Camera,
+        Audio,
+        VFX,
+        AI
+    }
+}
