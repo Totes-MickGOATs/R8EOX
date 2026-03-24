fa1ecc0e03b2399c7877fa3664a034319a281d5a
Adam McCune <totesmickgoats@Mac.home>
2026-03-23 21:41:45 -0600

    fix: convert public fields to [SerializeField] private with properties in GlobalSettingsFile

--- Assets/Scripts/Settings/Internal/GlobalSettingsFile.cs
+++ Assets/Scripts/Settings/Internal/GlobalSettingsFile.cs
@@ -5,7 +5,10 @@ namespace R8EOX.Settings.Internal
     [Serializable]
     internal class GlobalSettingsFile
     {
-        public VideoSettings video = new VideoSettings();
-        public ProfileSettings profiles = new ProfileSettings();
+        [SerializeField] private VideoSettings video = new VideoSettings();
+        [SerializeField] private ProfileSettings profiles = new ProfileSettings();
+
+        public VideoSettings Video { get => video; set => video = value; }
+        public ProfileSettings Profiles { get => profiles; set => profiles = value; }
     }
 }
